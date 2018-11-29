8b9e8a679e310850c97088784e32040a66a6a3ce
William Andrus <andrus_wj@hotmail.com>
2018-11-29 15:25:17 -0700

    Initial Upload

---
+++ circleCard/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var circleCard31D39E2DD8264575A5BA650637BFFB42 = {
+        name: 'circleCard31D39E2DD8264575A5BA650637BFFB42',
+        displayName: 'Circle Card',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.2.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.circleCard31D39E2DD8264575A5BA650637BFFB42.Visual(options),
+        custom: true
+    };
+}
